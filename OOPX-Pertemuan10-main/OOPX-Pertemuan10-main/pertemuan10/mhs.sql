69f3b8a3e8b7f56999bceb746d100ef5f26b38a0
kinokesu2004 <kinokesu2004@gmail.com>
2025-06-14 17:21:39 +0800

    14juni

---
+++ OOPX-Pertemuan10-main/OOPX-Pertemuan10-main/pertemuan10/mhs.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2025 at 09:38 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhs`
+create database mahasiswa;
+use mahasiswa;
+
+CREATE TABLE `mhs` (
+  `nama` varchar(30) NOT NULL,
+  `nim` varchar(10) NOT NULL,
+  `prodi` enum('IF','DKV','SK','BD') NOT NULL,
+  `jkel` enum('L','P') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mhs`
+--
+
+INSERT INTO `mhs` (`nama`, `nim`, `prodi`, `jkel`) VALUES
+('Ari Suhadi', '2301010343', 'DKV', 'L'),
+('Adolfridus Juniarius Darminto ', '2301010361', 'IF', 'L');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mhs`
+--
+ALTER TABLE `mhs`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ OOPX-Pertemuan10-main/OOPX-Pertemuan10-main/pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,16 @@
+package pertemuan10;
+
+/**
+ *
+ * @author INSTIKI
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
